0e55e1cc6183edc94a45244666cbda96be034994
João Victor <joao.victor@bymycell.com>
2024-03-18 10:05:52 -0300

    fix: apply eslint rules

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import Fastify from "fastify";
+import Fastify from 'fastify'
 
 const app = Fastify()
 
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   return 'hello world'
 })
 
-app.listen({
-    port: 3333
-}).then(() => {
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
     console.log('🔥 HTTP Server running on port 3333')
-})
+  })
