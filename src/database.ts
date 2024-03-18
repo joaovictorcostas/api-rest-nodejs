7ac45b274807125d4b680c24b9d8e1d03eda858f
João Victor <joao.victor@bymycell.com>
2024-03-18 10:45:17 -0300

    feat: create and export config

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,11 @@
 import { knex as setupKnex } from 'knex'
 
-export const knex = setupKnex({
+export const config = {
   client: 'sqlite3',
   connection: {
     filename: './tmp/app.sqlite',
   },
   useNullAsDefault: true,
-})
+}
+
+export const knex = setupKnex(config)
