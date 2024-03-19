7a980d0557410a5f3909e202ece072d1ab932230
João Victor <joao.victor@bymycell.com>
2024-03-19 08:28:48 -0300

    feat: add env variable verification

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,14 @@
+import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL must be set')
+}
 
 export const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './db/app.sqlite',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
