f0176f977c5a00c2e359ba02fcb901cda0a74512
João Victor <joao.victor@bymycell.com>
2024-03-19 10:35:35 -0300

    feat: create route to get transaction data by id and route to summarize values between transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,6 +3,35 @@ import { knex } from '../database'
 import { z } from 'zod'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async (request, reply) => {
+    const transactions = await knex('transactions').select('*')
+
+    return reply.status(200).send({ transactions })
+  })
+
+  app.get('/:id', async (request, reply) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions')
+      .select('*')
+      .where('id', id)
+      .first()
+
+    return reply.status(200).send({ transaction })
+  })
+
+  app.get('/summary', async (request, reply) => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return reply.status(200).send({ summary })
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
